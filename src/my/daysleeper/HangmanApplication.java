3ef3c0bbfc5312a0c593fb1ac14b0afab3e5aef8
Daysleeper <pgianelos@gmail.com>
2019-05-12 19:25:25 +0300

    HangMan - One round game

--- src/my/daysleeper/HangmanApplication.java
+++ src/my/daysleeper/HangmanApplication.java
@@ -1,24 +1,32 @@
 package my.daysleeper;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.security.SecureRandom;
 
 public class HangmanApplication {
 
-    private static final String[] WORDS = {"HELLO", "APPLAUSE", "BEAUTIFUL"};
+    private static final String[] WORDS = {"HELLO", "APPLAUSE", "BEAUTIFUL", "MAVERICK"};
+
+    private static SecureRandom random = new SecureRandom();
 
     public static void main(String[] args) {
 
-        List<Integer> points = Arrays.stream(WORDS).map(word -> {
-            System.out.println("NEW GAME");
-            return new HangMan(word, 3).play();
-        }).collect(Collectors.toList());
+        System.out.println("Welcome to HangMan!");
 
-        System.out.println("\n\nResults of all " + WORDS.length + " plays:");
-        points.forEach(point -> {
-            System.out.println(point + " points");
-        });
-        System.out.println("\nYou gathered from all games: " + points.stream().reduce((p1,p2) -> p1 + p2).get() + " points");
+        String word = WORDS[random.nextInt(3)];
+        int points = new HangMan(word, 5).play();
+
+        System.out.println("\nYou scored " + points + " points");
+
+
+//        List<Integer> points = Arrays.stream(WORDS).map(word -> {
+//            System.out.println("NEW GAME");
+//            return new HangMan(word, 3).play();
+//        }).collect(Collectors.toList());
+//
+//        System.out.println("\n\nResults of all " + WORDS.length + " plays:");
+//        points.forEach(point -> {
+//            System.out.println(point + " points");
+//        });
+//        System.out.println("\nYou gathered from all games: " + points.stream().reduce((p1,p2) -> p1 + p2).get() + " points");
     }
 }
